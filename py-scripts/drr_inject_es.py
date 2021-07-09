197baab2ed225b1cf12bd76f6c255daf1ec124be
Kristian Gade Kjelmann <kgk@adm.aau.dk>
2021-07-09 13:59:41 +0200

    filter troublesome ids

--- py-scripts/drr_inject_es.py
+++ py-scripts/drr_inject_es.py
@@ -8,10 +8,13 @@ import json
 
 with open("../data/drr_scrape2021-07-08_es.json.json", 'r') as f:
     data = json.load(f)
+
+error_ids = ['drmkc00188']
     
 docs = {}
 
 for item in data:
-    docs[item['id']] = item
+    if not item['id'] in error_ids:
+        docs[item['id']] = item
 
 drr_es_inject.inject(docs, index='drr', host='130.226.98.70', port=9200, verbose=True, verify=False)
